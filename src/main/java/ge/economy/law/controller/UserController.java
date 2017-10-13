930dc0aa449c0acdb0fb36c629c4b48ebb5070d7
Ucha <uchachaduneli@gmail.com>
2017-10-13 16:34:39 +0400

    first build

--- src/main/java/ge/economy/law/controller/UserController.java
+++ src/main/java/ge/economy/law/controller/UserController.java
@@ -1,9 +1,11 @@
 package ge.economy.law.controller;
 
+import ge.economy.law.misc.Response;
 import ge.economy.law.service.UsersService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 
 /**
@@ -16,11 +18,10 @@ public class UserController {
     @Autowired
     private UsersService usersService;
 
-//    @RequestMapping("/get-mobile-users")
-//    @ResponseBody
-//    private List<MobileUsersDTO> getMobileUsers() throws Exception {
-////        return usersService.getMobileUsers();
-//        return null;
-//    }
+    @RequestMapping("/get-users")
+    @ResponseBody
+    private Response getUsers() throws Exception {
+        return Response.withSuccess(usersService.getUsers());
+    }
 
 }
