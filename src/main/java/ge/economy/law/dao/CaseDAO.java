94c49dd1760f2bcf02096d301c9963de53050662
Ucha <uchachaduneli@gmail.com>
2017-11-02 11:57:57 +0400

    first build

--- src/main/java/ge/economy/law/dao/CaseDAO.java
+++ src/main/java/ge/economy/law/dao/CaseDAO.java
@@ -1,6 +1,7 @@
 package ge.economy.law.dao;
 
 import ge.economy.law.model.Tables;
+import ge.economy.law.model.tables.Case;
 import ge.economy.law.model.tables.records.CaseRecord;
 import org.jooq.Record;
 import org.jooq.SelectOnConditionStep;
@@ -10,7 +11,6 @@ import java.util.HashMap;
 import java.util.List;
 
 import static org.jooq.impl.DSL.max;
-import static org.jooq.impl.DSL.row;
 
 /**
  * Created by ME.
@@ -20,31 +20,30 @@ import static org.jooq.impl.DSL.row;
 public class CaseDAO extends AbstractDAO {
 
     public HashMap<String, Object> getCases(int start, int limit) {
-        SelectOnConditionStep<Record> selectConditionStep =
-                dslContext.select()
-                        .from(Tables.CASE)
-                        .join(Tables.JUDGE)
-                        .on(Tables.CASE.JUDGE_ID.eq(Tables.JUDGE.JUDGE_ID))
-                        .join(Tables.LITIGATION_SUBJECT)
-                        .on(Tables.CASE.LITIGATION_SUBJECT_ID.eq(Tables.LITIGATION_SUBJECT.LITIGATION_SUBJECT_ID))
-                        .join(Tables.END_RESULT)
-                        .on(Tables.CASE.END_RESULT_ID.eq(Tables.END_RESULT.END_RESULT_ID))
-                        .join(Tables.COURT)
-                        .on(Tables.CASE.COURT_ID.eq(Tables.COURT.COURT_ID))
-                        .join(Tables.USER)
-                        .on(Tables.CASE.ADD_USER_ID.eq(Tables.USER.USER_ID))
-                        .join(Tables.STATUS)
-                        .on(Tables.CASE.STATUS_ID.eq(Tables.STATUS.STATUS_ID));
+        Case c = Tables.CASE.as("c");
+        Case d = Tables.CASE.as("d");
 
-        selectConditionStep.where(Tables.CASE.CASE_ID.eq(
-                dslContext.
-                        select(max(Tables.CASE.CASE_ID))
-                        .from(Tables.CASE).
-                        where(Tables.CASE.NUMBER.eq(row(Tables.CASE.NUMBER))).fetchOne()));
+        SelectOnConditionStep<Record> selectConditionStep = (SelectOnConditionStep<Record>) dslContext.select()
+                .from(c)
+                .join(Tables.LITIGATION_SUBJECT)
+                .on(c.LITIGATION_SUBJECT_ID.eq(Tables.LITIGATION_SUBJECT.LITIGATION_SUBJECT_ID))
+                .join(Tables.END_RESULT)
+                .on(c.END_RESULT_ID.eq(Tables.END_RESULT.END_RESULT_ID))
+                .join(Tables.COURT)
+                .on(c.COURT_ID.eq(Tables.COURT.COURT_ID))
+                .join(Tables.USER)
+                .on(c.ADD_USER_ID.eq(Tables.USER.USER_ID))
+                .join(Tables.JUDGE)
+                .on(c.JUDGE_ID.eq(Tables.JUDGE.JUDGE_ID))
+                .join(Tables.STATUS)
+                .on(c.STATUS_ID.eq(Tables.STATUS.STATUS_ID))
+                .where(c.CASE_ID.eq(dslContext.select(max(Tables.CASE.CASE_ID)).from(Tables.CASE)
+                        .where(Tables.CASE.NUMBER.eq(c.NUMBER))));
+
 
         SelectOnConditionStep<Record> selectConditionStepSize = selectConditionStep;
         int recordSize = selectConditionStepSize.fetch().size();
-        selectConditionStep.orderBy(Tables.CASE.CASE_ID.desc()).limit(limit).offset(start);
+        selectConditionStep.orderBy(c.CASE_ID.desc()).limit(limit).offset(start);
 
         HashMap<String, Object> map = new HashMap();
         map.put("list", selectConditionStep.fetch());
